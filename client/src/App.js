1c08546ce89e800d2a34406c0cd137f207a11038
pamuditha <pamudithasenanayake@gmail.com>
2024-08-26 19:40:49 +0530

    home page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import MainPage from './pages/MainPage';
+import Home from './pages/pamuditha/home'; 
 
 function App() {
   return (
-    <div>
-      <MainPage/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
