9bbe830aad939c66eb069fab5eeef3b4fd010569
pamuditha <pamudithasenanayake@gmail.com>
2024-08-27 11:10:11 +0530

    Login UI created. added as localhost:3000/

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import MainPage from './pages/MainPage';
-import Home from './pages/pamuditha/home'; 
+import Home from './pages/pamuditha/home';
+import Test1 from './pages/pamuditha/test1';
+import Test2 from './pages/pamuditha/test2';
+// import SP from './pages/navodya/SelectServices';
+import ReactDOM from "react-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainPage />} />
         <Route path="/home" element={<Home />} />
+          <Route path="/test1" element={<Test1 />} />
+          <Route path="/" element={<Test2 />} />
+          {/*<Route path="/test3" element={<SP />} />*/}
+
       </Routes>
     </Router>
   );
