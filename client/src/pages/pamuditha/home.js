8e4dd7da102c74e4c8f4f48c8366739554a350a4
pamuditha <pamudithasenanayake@gmail.com>
2024-08-28 23:14:32 +0530

    nav bar colors

--- client/src/pages/pamuditha/home.js
+++ client/src/pages/pamuditha/home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../images/logo.jpeg";
+import logo2 from "../../images/logow.png";
 import homepic from "../../images/home.jpg";
 
 // Function to smoothly scroll to a section
@@ -14,14 +15,14 @@ function Navbar() {
 
 
   return (
-      <nav className="bg-white fixed top-0 left-0 w-full z-50 bg-white shadow-md">
+      <nav className="bg-black fixed top-0 left-0 w-full z-50 shadow-md">
         <div className="w-full pr-5">
             <div className="flex flex-row h-20 justify-between">
                 {/* Left side with Logo */}
 
-                <div className="flex-shrink-0 content-start">
+                <div className="flex-shrink-0 content-start pl-4">
                     <a href="/home">
-                        <img src={logo} alt="Logo" className="w-[14%] start content-start"/>
+                        <img src={logo2} alt="Logo" className="h-full start content-start"/>
                     </a>
                 </div>
                 <div className="flex items-center content-end">
@@ -29,33 +30,33 @@ function Navbar() {
                     <div className="hidden md:block">
                         <div className="ml-10 flex items-baseline space-x-4">
                             <button onClick={() => scrollToSection("home")}
-                                    className="text-black hover:bg-black hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home
+                                    className="julius-sans-one-regular text-white hover:bg-pink-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home
                             </button>
                             <button onClick={() => scrollToSection("gallery")}
-                                    className="text-black hover:bg-black hover:text-white px-3 py-2 rounded-md text-sm font-medium">Gallery
+                                    className="julius-sans-one-regular text-white hover:bg-pink-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Gallery
                             </button>
                             <button onClick={() => scrollToSection("testimonials")}
-                                    className="text-black hover:bg-black hover:text-white px-3 py-2 rounded-md text-sm font-medium">Testimonials
+                                    className="julius-sans-one-regular text-white hover:bg-pink-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Testimonials
                             </button>
                             <button onClick={() => scrollToSection("about")}
-                                    className="ttext-black hover:bg-black hover:text-white px-3 py-2 rounded-md text-sm font-medium">About
+                                    className="julius-sans-one-regular text-white hover:bg-pink-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About
                                 Us
                             </button>
                             <button onClick={() => scrollToSection("profile")}
-                                    className="ttext-black hover:bg-black hover:text-white px-3 py-2 rounded-md text-sm font-medium">Profile
+                                    className="julius-sans-one-regular text-white hover:bg-pink-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Profile
                             </button>
                             <Link to="/appointments"
-                                  className="text-black hover:bg-red-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Appointments</Link>
+                                  className="julius-sans-one-regular text-white hover:bg-red-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Appointments</Link>
 
                             <div>
 
-                                    <a className="nav-link dropdown-toggle" href="http://example.com" id="dropdown07"
+                                    <a className="nav-link text-white julius-sans-one-regular dropdown-toggle" href="http://example.com" id="dropdown07"
                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Menu</a>
                                 <div className="dropdown-menu" aria-labelledby="dropdown07">
-                                    <a className="dropdown-item" href="#">Cart</a>
-                                    <a className="dropdown-item" href="#">Login</a>
-                                    <a className="dropdown-item" href="#">Logout</a>
-                                    <a className="dropdown-item" href="#">Profile</a>
+                                    <a className="dropdown-item julius-sans-one-regular" href="#">Cart</a>
+                                    <a className="dropdown-item julius-sans-one-regular" href="#">Login</a>
+                                    <a className="dropdown-item julius-sans-one-regular" href="#">Logout</a>
+                                    <a className="dropdown-item julius-sans-one-regular" href="#">Profile</a>
                                 </div>
 
                             </div>
