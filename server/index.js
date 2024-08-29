4e78a3bd7f788e423532b4e96c729d6beac9eb4c
Navodya Dhanushka <navodyadhanushka01@gmail.com>
2024-08-30 02:20:58 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,17 +1,24 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import 'express-async-errors';
-import ErrorHandler from "./middlewares/ErrorHandler.js";
+import cors from 'cors';
 import db from './db.js';
+import ErrorHandler from "./middlewares/ErrorHandler.js";
 import employeeRoutes from './controllers/employee.controller.js';
-import userPM from './controllers/userManagement.js';
+import beautyServicesRoutes from './controllers/beautyService.controller.js';
+import professionalRoutes from './controllers/professional.controller.js';
+
+// import userPM from './controllers/userManagement.js';
 
 const app = express();
+app.use(cors());
 
 // Middleware
 app.use(bodyParser.json());
 app.use('/api/employees', employeeRoutes);
-app.use('/api/users', userPM);
+app.use('/api/beautyservices', beautyServicesRoutes);
+app.use('/api/selectprofessional', professionalRoutes);
+// app.use('/api/users', userPM);
 app.use(ErrorHandler)
 
 
@@ -19,7 +26,7 @@ app.use(ErrorHandler)
 db.query("SELECT 1")
     .then(() => {
         console.log('DB connection succeeded.');
-        app.listen(3000, () => console.log('Server started at port 3000'));
+        app.listen(3001, () => console.log('Server started at port 3001'));
     })
     .catch(err => console.log('DB connection failed.\n' + err));
 
