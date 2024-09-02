59d3703c47f76a13c4a0d1954aa9fee37e496229
sasanka dilum chamuditha <165694341+sasankadilum@users.noreply.github.com>
2024-09-02 21:59:02 +0530

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,9 @@ import Test2 from './pages/pamuditha/test2';
 import Test3 from './pages/navodya/SelectServices';
 import Test4 from './pages/navodya/SelectProfessional';
 // import SP from './pages/navodya/SelectServices';
+import S from './pages/sasanka/s';
+import Cart from './pages/sasanka/Cart';
+import Checkout from "./pages/sasanka/Checkout";
 import ReactDOM from "react-dom";
 
 function App() {
@@ -14,8 +17,11 @@ function App() {
     <Router>
       <Routes>
         <Route path="/home" element={<Home />} />
-          <Route path="/test1" element={<Test1 />} />
-          <Route path="/" element={<Test2 />} />
+          <Route path="/test1" element={<S />} />
+          <Route path="/Test3" element={<Test3 />} />
+          <Route path="/s" element={<S />} />
+          <Route path="/Cart" element={<Cart />} />
+          <Route path="/Checkout" element={<Checkout/>}/>
           {/*<Route path="/test3" element={<SP />} />*/}
 
       </Routes>
