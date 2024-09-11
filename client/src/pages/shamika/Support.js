1e3e215e5c44b36d2c5d2a2c511cfa491d5671a0
shamika <shamikavin@gmail.com>
2024-09-11 19:36:11 +0530

    css  modifications

--- client/src/pages/shamika/Support.js
+++ client/src/pages/shamika/Support.js
@@ -1,61 +1,66 @@
 import React from "react";
-import backgroundImage from "../../images/a2.jpg";
+import backgroundImage from "../../images/5.jpg";
 
 
 export default function Support() {
     return (
 
         <div
-            className="flex flex-col items-center justify-center h-screen bg-pink 400"
-            style={{backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center'}}
+            className="flex flex-col items-center justify-center h-screen bg-white"
+            style={{
+                backgroundImage: `url(${backgroundImage})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+            }}
         >
             <div className="flex flex-row w-[70%] h-[600px] bg-opacity-70">
-                <div className="flex flex-col w-[50%] h-full justify-center pl-9"
-                     style={{
-                         background: 'rgba(87, 40, 215, 0.2)',
-                         borderRadius: '16px',
-                         boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
-                         backdropFilter: 'blur(5px)',
-                         WebkitBackdropFilter: 'blur(5px)',
-                         border: '1px solid rgba(255, 255, 255, 0.3)',
-                     }}
+                <div
+                    className="left-div flex flex-col w-[60%] h-full justify-center pl-6 pamlogin1"
+                    style={{
+                        background: 'rgba(87, 40, 215, 0.2)',
+                        borderRadius: '0',
+                        boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
+                        backdropFilter: 'blur(5px)',
+                        WebkitBackdropFilter: 'blur(5px)',
+                        border: '1px solid rgba(255, 255, 255, 0.3)',
+                    }}
                 >
-                    <h1 className="lg:mx-32 text-5xl  julius-sans-one-regular font-bold text-orange-500">Customer
-                        Support</h1>
-                    <h3 className="lg:mx-32 text-2xl  julius-sans-one-regular opacity-40 py-6">Welcome to Our Customer
-                        Support!</h3>
 
-                    <p className="lg:mx-32  julius-sans-one-regular opacity-30 py-1">Thank you for reaching out to us.
+                    <h1 className="lg:mx-20 text-3xl  justify-center text-white julius-sans-one-regular font-bold mb-10">Customer
+                        Support</h1>
+                    <h3 className="lg:mx-20 text-1xl  text-white julius-sans-one-regular mb-4 ">Welcome to
+                        Our Customer  Support!</h3>
+
+                    <p className="flex 1 lg:mx-20  julius-sans-one-regular text-1xl text-rgb255 mb-4">Thank you for reaching
+                        out to us.
                         At Salon Diamond, we are dedicated to providing you with the best possible service and support.
                         Whether you have questions, need assistance, our team is here to help you every step of the
                         way.</p>
 
-                    <p className="lg:mx-32  julius-sans-one-regular opacity-30 py-1">Please explore our resources and
+                    <p className="lg:mx-20  julius-sans-one-regular  text-1xl text-rgb255 mb-4">Please explore our
+                        resources and
                         get in touch with us through the contact options available.
                         We value your time and are committed to ensuring your experience with us is nothing short of
                         exceptional.</p>
 
-                    <p className="lg:mx-32  julius-sans-one-regular opacity-30 py-2">Get started!</p>
+                    <p className="lg:mx-20  julius-sans-one-regular  text-2xl text-rgb255 font-bold mb-5 ">Get started!</p>
+                    <div className="flex space-x-4 pl-12">
+                    <a
+                        href="/ticket"
+                        className="flex  items-right justify-center h-10 julius-sans-one-regular w-25 bg-pink-500 text-white border-[1px]  rounded-lg  transition-transform transform hover:translate-y-[-2px] hover:shadow-xl hover:translate-x-[-5px]"
+                    >
+                        New Ticket
+                    </a>
 
                     <a
                         href="/ticket"
-                        className="lg:mx-32 bg-yellow-600 hover:bg-amber-200  julius-sans-one-regular text-black font-medium py-3 px-6 rounded-md"
+                        className="flex items-center justify-center h-10 julius-sans-one-regular w-25 bg-pink-500 text-white border-[1px]  rounded-lg  transition-transform transform hover:translate-y-[-2px] hover:shadow-xl hover:translate-x-[-5px]"
                     >
-                        NEW SUPPORT TICKET
+                        My Ticket
                     </a>
-
-
-                    <button
-                        className="w-full py-2 px-4 julius-sans-one-regular bg-neutral-800  julius-sans-one-regular text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    >{""}MY TICKET
-
-                    </button>
-
-
+                    </div>
                 </div>
             </div>
         </div>
-
-
-    )
-}
+    );
+};
--- client/src/pages/shamika/Ticket.js
+++ client/src/pages/shamika/Ticket.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import backgroundImage from "../../images/a2.jpg";
+import backgroundImage from "../../images/5.jpg";
 
 function Ticket() {
 
@@ -38,91 +38,118 @@ function Ticket() {
     };
 
     return (
+
         <div
             className="flex flex-col items-center justify-center h-screen bg-white"
-            style={{backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center'}}
+            style={{
+                backgroundImage: `url(${backgroundImage})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+            }}
         >
             <div className="flex flex-row w-[70%] h-[600px] bg-opacity-70">
-                <div className="flex flex-col w-[50%] h-full justify-center pl-9"
-                     style={{
-                         background: 'rgba(87, 40, 215, 0.2)',
-                         borderRadius: '16px',
-                         boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
-                         backdropFilter: 'blur(5px)',
-                         WebkitBackdropFilter: 'blur(5px)',
-                         border: '1px solid rgba(255, 255, 255, 0.3)',
-                     }}
+                <div
+                    className="left-div flex flex-col w-[75%] h-full justify-center pl-6 pamlogin1"
+                    style={{
+                        background: 'rgba(87, 40, 215, 0.2)',
+                        borderRadius: '0',
+                        boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
+                        backdropFilter: 'blur(5px)',
+                        WebkitBackdropFilter: 'blur(5px)',
+                        border: '1px solid rgba(255, 255, 255, 0.3)',
+                    }}
                 >
+
                     <div className="App">
-                        <h1>New Customer Support Ticket</h1>
-                        <form onSubmit={handleSubmit}>
-                            <div>
-                                <label>Customer ID:</label>
-                                <input
-                                    type="text"
-                                    name="customer_id"
-                                    value={ticket.customer_id}
-                                    onChange={handleChange}
-                                    required
-                                />
-                            </div>
-                            <div>
-                                <label>Email:</label>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    value={ticket.email}
-                                    onChange={handleChange}
-                                    required
-                                />
-                            </div>
-                            <div>
-                                <label>Contact No:</label>
-                                <input
-                                    type="text"
-                                    name="contact_no"
-                                    value={ticket.contact_no}
-                                    onChange={handleChange}
-                                    required
-                                />
-                            </div>
-                            <div>
-                                <label>Category:</label>
-                                <select
-                                    name="category"
-                                    value={ticket.category}
-                                    onChange={handleChange}
-                                >
-                                    <option value="Booking and Appointment Issues">Booking and Appointment Issues
-                                    </option>
-                                    <option value="Payment and Billing Concerns">Payment and Billing Concerns</option>
-                                    <option value="Service-Related Complaints">Service-Related Complaints</option>
-                                    <option value="Technical Problems with the Online Platform">Technical Problems with
-                                        the Online Platform
-                                    </option>
-                                    <option value="Product Inquiries and Issues">Product Inquiries and Issues</option>
-                                </select>
-                            </div>
-                            <div>
-                                <label>Inquiry Description (Max 100 characters):</label>
-                                <input
-                                    type="text"
-                                    name="inquiry_description"
-                                    value={ticket.inquiry_description}
-                                    onChange={handleChange}
-                                    maxLength="100"
-                                    required
-                                />
-                            </div>
-                            <button type="submit">Submit</button>
-                            <button type="button" onClick={handleReset}>Reset</button>
-                        </form>
+                        <h1 className="lg:mx-20   justify-center julius-sans-one-regular  text-3xl font-bold text-rgb255 mb-4">New
+                            Customer
+                            Support Ticket
+                        </h1>
+                        <div className="lg:mx-20 text-1xl  justify-right  text-black julius-sans-one-regular font-bold mb-12">
+                            <form onSubmit={handleSubmit}>
+                                <div>
+                                    <label>Customer ID:</label>
+                                    <input
+                                        type="text"
+                                        name="customer_id"
+                                        value={ticket.customer_id}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+
+                                <div>
+                                    <label>Email:</label>
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        value={ticket.email}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <label>Contact No:</label>
+                                    <input
+                                        type="text"
+                                        name="contact_no"
+                                        value={ticket.contact_no}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <label>Category:</label>
+                                    <select
+                                        name="category"
+                                        value={ticket.category}
+                                        onChange={handleChange}
+                                    >
+                                        <option value="Booking and Appointment Issues">Booking and Appointment Issues
+                                        </option>
+                                        <option value="Payment and Billing Concerns">Payment and Billing Concerns
+                                        </option>
+                                        <option value="Service-Related Complaints">Service-Related Complaints</option>
+                                        <option value="Technical Problems with the Online Platform">Technical Problems
+                                            with
+                                            the Online Platform
+                                        </option>
+                                        <option value="Product Inquiries and Issues">Product Inquiries and Issues
+                                        </option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label>Inquiry Description (Max 100 characters):</label>
+                                    <input
+                                        type="text"
+                                        name="inquiry_description"
+                                        value={ticket.inquiry_description}
+                                        onChange={handleChange}
+                                        maxLength="100"
+                                        required
+                                    />
+                                </div>
+                                <div className="flex space-x-4 pl-12">
+                                    <a
+                                        href="/ticket"
+                                        className="flex  items-right justify-center h-10 julius-sans-one-regular w-25 bg-black text-white border-[1px]  rounded-lg  transition-transform transform hover:translate-y-[-2px] hover:shadow-xl hover:translate-x-[-5px]"
+                                    >
+                                        Submit
+                                    </a>
+                                    <a
+                                        href="/ticket"
+                                        className="flex  items-right justify-center h-10 julius-sans-one-regular w-25 bg-black text-white border-[1px]  rounded-lg  transition-transform transform hover:translate-y-[-2px] hover:shadow-xl hover:translate-x-[-5px]"
+                                    >
+                                        Reset
+                                    </a>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+    );
+}
 
-                    );
-                    }
-
-                    export default Ticket;
+export default Ticket;
