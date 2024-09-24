11b434e510147d874cb29fff2da46df26343b85e
pamuditha <pamudithasenanayake@gmail.com>
2024-09-24 22:44:40 +0530

    correct the google login error

--- server/controllers/userManagement.js
+++ server/controllers/userManagement.js
@@ -144,9 +144,10 @@ router.post("/register", async (req, res) => {
                     res.status(500).json({message: "Server error during registration"});
                 } else {
                     const result = await db.query(
-                        "INSERT INTO users (email, password, role) VALUES ($1, $2, $3) RETURNING *",
+                        "INSERT INTO users (email, password, role, date) VALUES ($1, $2, $3, NOW()) RETURNING *",
                         [email, hash, role]
                     );
+                    console.log(result);
                     const user = result.rows[0];
 
                     req.login(user, (err) => {
