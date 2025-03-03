437b3d6a2d1c5b1ab062b3b71831470a869f68f5
pamuditha <pamudithasenanayake@gmail.com>
2025-03-03 15:32:58 +0530

    nav bar reponse fix 1

--- client/src/pages/pamuditha/nav.js
+++ client/src/pages/pamuditha/nav.js
@@ -1,32 +1,34 @@
 import React from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import {Link, useLocation, useNavigate} from 'react-router-dom';
 import Cookies from 'js-cookie';
-import { useLogout } from './authUtils';
+import {useLogout} from './authUtils';
 import logo2 from "../../images/logow.png";
 import cartIcon from '../../images/cart.png';
 
-function Navbar({ cartCount }) { // Accept cartCount as a prop
+function Navbar({cartCount}) { // Accept cartCount as a prop
     const logout = useLogout(); // Using the custom hook
     const [cookieExists, setCookieExists] = React.useState(false);
     const navigate = useNavigate();
     const location = useLocation();
 
     React.useEffect(() => {
-        const cookie = Cookies.get('diamond');
-        setCookieExists(!!cookie);
+        const checkCookie = () => {
+            const cookie = Cookies.get('diamond');
+            setCookieExists(!!cookie);
+        };
 
-        // Detect section to scroll after navigating from another page
-        const hash = location.hash;
-        if (hash) {
-            const sectionId = hash.replace('#', '');
-            scrollToSection(sectionId);
-        }
-    }, [location]);
+        checkCookie(); // Check on component mount
+
+        const interval = setInterval(checkCookie, 5000); // Check every 5s
+
+        return () => clearInterval(interval); // Cleanup on unmount
+    }, []);
+
 
     const scrollToSection = (id) => {
         const element = document.getElementById(id);
         if (element) {
-            element.scrollIntoView({ behavior: 'smooth' });
+            element.scrollIntoView({behavior: 'smooth'});
         }
     };
 
@@ -47,14 +49,14 @@ function Navbar({ cartCount }) { // Accept cartCount as a prop
     return (
         <nav
             className="bg-black fixed top-0 left-1/2 transform -translate-x-1/2 w-[97%] z-50 shadow-md mt-2"
-            style={{ borderRadius: 40 }}
+            style={{borderRadius: 40}}
         >
             <div className="w-full pr-5">
                 <div className="flex flex-row h-20 justify-between">
                     {/* Left side with Logo */}
                     <div className="flex-shrink-0 content-start pl-4">
                         <a href="/home">
-                            <img src={logo2} alt="Logo" className="h-full start content-start" />
+                            <img src={logo2} alt="Logo" className="h-full start content-start"/>
                         </a>
                     </div>
 
@@ -87,12 +89,14 @@ function Navbar({ cartCount }) { // Accept cartCount as a prop
                                     Products
                                 </Link>
                                 <Link to="/cart" className="relative">
-                                    <span className="julius-sans-one-regular text-white hover:bg-red-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                                    <span
+                                        className="julius-sans-one-regular text-white hover:bg-red-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
                                         Cart |
-                                        <img src={cartIcon} alt="Cart" className="w-4 h-4 inline-block" />
+                                        <img src={cartIcon} alt="Cart" className="w-4 h-4 inline-block"/>
                                     </span>
                                     {cookieExists && (
-                                        <span className="absolute -top-2 -right-2 bg-pink-600 text-white rounded-full px-1 text-xs">
+                                        <span
+                                            className="absolute -top-2 -right-2 bg-pink-600 text-white rounded-full px-1 text-xs">
                                             {cartCount}
                                         </span>
                                     )}
